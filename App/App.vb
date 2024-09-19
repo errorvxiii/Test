f6e23054c0c5eb44ff412e35557faab5da08647a
errorvxii <alisahb2900@gmail.com>
2024-09-19 11:37:35 +0330

    Salam

---
+++ App/App.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ACMApp1 As Object
+        ACMApp1 = GetObject("C:\Users\Ali\Desktop\kir\kir.dynf")
+        ACMApp1.Application.Quit
+    End Sub
+
+End Module
